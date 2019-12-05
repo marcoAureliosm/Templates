0a3dd0f89c6c77fbf75033afe67e0027f562bdd8
Marco Aurelio <marco.truvo@gmail.com>
2019-12-05 09:04:57 -0300

    Atividade Templates

---
+++ mysite/core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(
+        request,
+        'index.html',
+        {'titulo':'Meu Titulo'}
+    )
+# Create your views here.
